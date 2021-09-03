98898ea041f7e7dd16aad4f922a7916fdf17f1a2
youchenlong <54472586+youchenlong@users.noreply.github.com>
2021-09-03 01:18:48 -0700

    Add files via upload

--- test/test.c
+++ test/test.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-#include<stdint.h>
-
-int main(){
-    if(-INT32_MAX-1 < INT32_MAX){
-        printf("true");
-    }
-    else{
-        printf("false");
-    }
-    return 0;
-}
+#include<pthread.h>
