b169b5dc74af948120f28ff7be353f7f03ed209c
youchenlong <54472586+youchenlong@users.noreply.github.com>
2021-09-07 15:13:39 +0800

    Add files via upload

--- test/test.c
+++ test/test.c
@@ -1,1 +1,13 @@
-#include<pthread.h>
+#include<stdio.h>
+#define N 10
+int main(){
+    char a[N];
+    char b[N];
+    printf("input a: ");
+    scanf("%s", &a);
+    printf("input b: ");
+    scanf("%s", &b);
+    printf("%s\n", a);
+    printf("%s\n", b);
+    return 0;
+}
